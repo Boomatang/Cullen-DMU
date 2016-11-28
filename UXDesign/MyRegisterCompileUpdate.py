b8259c56bc3e9f6c769c6f80824090b942517424
boomatang <jimfity@gmail.com>
2016-11-28 23:10:38 +0000

    Up date window feed back
    An update window UI has been done but the function has not yet been done. This is going
    to be left off till a later date.

---
+++ UXDesign/MyRegisterCompileUpdate.py
@@ -0,0 +1,26 @@
+import sys
+from PyQt4 import QtGui
+import UXDesign.RegisterCompileUpdate as RegisterCompileUpdate
+
+
+class RegisterCompileUpdateClass(QtGui.QDialog, RegisterCompileUpdate.Ui_Dialog):
+    def __init__(self, parent=None):
+        super(RegisterCompileUpdateClass, self).__init__(parent)
+        self.setupUi(self)
+        self.button_actions()
+
+    def button_actions(self):
+        # self.register_documnet.clicked.connect(self.get_register_document)
+        # self.project_folder.clicked.connect(self.get_project_folder)
+        # self.compile_to_folder.clicked.connect(self.get_destination_folder)
+        # self.compile_package.clicked.connect(self.run)
+        pass
+
+    def run(self):
+        print('I am running and I\'m the update')
+
+if __name__ == "__main__":
+    app = QtGui.QApplication(sys.argv)
+    ex = RegisterCompileUpdateClass()
+    ex.show()
+    sys.exit(app.exec_())
